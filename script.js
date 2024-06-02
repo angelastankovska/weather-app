625cea3a1b6cc72f571e88ac6dc3dc096d71350b
angelastankovska <angela.stankovska27.11@gmail.com>
2024-06-02 15:46:46 +0200

    added changes to checkWeather()

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const apiKey ="4187b0a4a571cf614ca547e12a8d22cb";
-const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&p="
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 const searchBox= document.querySelector(".search input");
 const searchBtn= document.querySelector(".search button");
 const weatherIcon= document.querySelector(".weather-icon")
 async function checkWeather(city){
     const response=await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data=await response.json();
-    //console.log(data);
+    console.log(data);
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp)+"°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity+"%";
